6b64772029399386aeaca4a1af6651e5695a7b82
akj1991 <akhilonattu@gmail.com>
2022-02-01 16:49:12 +0530

    a4

--- chelseafc/urls.py
+++ chelseafc/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 urlpatterns = [
  path('1st/', views.samplefunction), 
- path('2nd/', views.londonfun)  
+ path('2nd/', views.londonfun), 
+ path('3rd/', views.hello) 
 ]
